cbeee2af59c5b5b88a31e5ed57cb6ee8ba2f0389
Sam Kim <sendtosamkim@gmail.com>
2021-02-11 23:03:39 -0500

    initial commit of index.js and modules

---
+++ index.js
@@ -0,0 +1,14 @@
+// npm init -y, npm install inquirer
+const inquirer = require('inquire');
+const fs = require('fs');
+
+// create standing README.md
+function makeREADME(userInput){
+    return ``
+}
+
+//SYNC way
+// 1. ask for input
+// 2. generate the readme.md
+// 3. writeFileSync
+// 4. console.log to user finish
